8797dee018a4fd460e1c3740e0491cd4ef192040
Firmware <ph1rmw4r3@gmail.com>
2023-08-06 06:46:47 -0500

    fix all market prices are first items value

--- spoopy/settings.py
+++ spoopy/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "buyback.apps.BuybackConfig",
-    "buyback.custom_filters",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
