34eb541932ab71b84f42d7b97cdd63baa5736e6c
Firmware <ph1rmw4r3@gmail.com>
2023-08-06 05:32:57 -0500

    get debug bool from env var

--- buyback/admin.py
+++ buyback/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
-
-# Register your models here.
-
-
-from django.contrib import admin
 from .models import EveItemTax
 
-
-
-
-# admin.site.register(EveItemTax)
-
-
 class EveItemTaxAdmin(admin.ModelAdmin):
-    search_fields = ['type_name', 'type_id', 'group_id']  # Add any other fields you want to search
-    list_display = ['type_name', 'type_id', 'group_id', 'jita_buy_percentage']  # Add 'jita_buy_percentage' here
+    search_fields = ['type_name', 'type_id', 'group_id']
+    list_display = ['type_name', 'type_id', 'group_id', 'jita_buy_percentage']
+    list_filter = ['group_id']
 
-# Register your model with the custom admin class
+    # Define a custom action to batch edit the jita_buy_percentage for an entire group
+    def batch_edit_jita_buy_percentage(self, request, queryset):
+        selected_group_id = queryset.first().group_id
+        new_jita_buy_percentage = float(request.POST.get('new_jita_buy_percentage'))
+        
+        # Update the jita_buy_percentage for the selected group
+        EveItemTax.objects.filter(group_id=selected_group_id).update(jita_buy_percentage=new_jita_buy_percentage)
+    
+    batch_edit_jita_buy_percentage.short_description = "Batch Edit Jita Buy Percentage"
+
+    # Add the custom action to the actions list
+    actions = [batch_edit_jita_buy_percentage]
+
 admin.site.register(EveItemTax, EveItemTaxAdmin)
