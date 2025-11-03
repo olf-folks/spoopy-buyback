d70f29f57f4b54e229ad5693ebb9764f4ef2f8c2
olf-folks <ph1rmw4r3@gmail.com>
2025-11-03 13:31:37 +0000

    modified:   config/settings/production.py Found hardcoded env var but did not fix it

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,8 +6,9 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "../", "mediafiles")
+
+# this should be env var
 STATIC_URL = "http://157.250.203.42/static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, "../", "staticfiles")
 
 
 
